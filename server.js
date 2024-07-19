fb6563bc0891ded7fae6f7f76f7ab5b48ef66713
SUNIL SURENDRAN <133184647+SunilSurendran1906@users.noreply.github.com>
2024-07-19 17:29:57 +0530

    commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cros from "cors";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
